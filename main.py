f763f83447448c54546ff13137a5edeee21dd75e
yankeecaesar0-lgtm <yankeecaesar0@gmail.com>
2026-05-04 14:47:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+TOKEN = os.getenv("8594936680:AAEA5qXLCbMRTp8TVUJz-C3Ti4cx_dIzAYQ")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Use /story or /startstory to begin!")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+
+app.run_polling()
