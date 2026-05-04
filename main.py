644498426f0daf77a5cc9319d391dacc24d03ae8
yankeecaesar0-lgtm <yankeecaesar0@gmail.com>
2026-05-05 00:56:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
-TOKEN = os.getenv("8594936680:AAEA5qXLCbMRTp8TVUJz-C3Ti4cx_dIzAYQ")
+TOKEN = os.getenv("TOKEN")
+
+if TOKEN is None:
+    raise ValueError("TOKEN environment variable not set")
+
+TOKEN = TOKEN.strip()
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Use /story or /startstory to begin!")
 
 
-app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
+
+    app.run_polling()
 
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
