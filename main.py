58a2edb0060b21454202e0c52ec1ae5af012f9e2
yankeecaesar0-lgtm <yankeecaesar0@gmail.com>
2026-05-05 15:35:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
-
+from flask import Flask
+from threading import Thread
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+web_app = Flask(__name__)
+
+@web_app.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    web_app.run(host="0.0.0.0", port=port)
 
 TOKEN = os.getenv("8594936680:AAEA5qXLCbMRTp8TVUJz-C3Ti4cx_dIzAYQ")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Use /story or /startstory to begin!")
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
