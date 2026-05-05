a246f76f3275242992a8f0c346a75dc8ad288e35
yankeecaesar0-lgtm <yankeecaesar0@gmail.com>
2026-05-05 15:41:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ def run_web():
 TOKEN = os.getenv("8594936680:AAEA5qXLCbMRTp8TVUJz-C3Ti4cx_dIzAYQ")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Use /story or /startstory to begin!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+Thread(target=run_web).start()
+app.run_polling()
