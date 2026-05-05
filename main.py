910f4d87e0ba28e64cf5a7590be87fba070eb780
yankeecaesar0-lgtm <yankeecaesar0@gmail.com>
2026-05-05 10:46:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import os
-import telegram
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Bot
 
-print("PTB version:", telegram.__version__)
+TOKEN = os.getenv("TOKEN")
 
-TOKEN = os.getenv("8594936680:AAEA5qXLCbMRTp8TVUJz-C3Ti4cx_dIzAYQ")
+print("TOKEN =", repr(TOKEN))
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Use /story or /startstory to begin!")
-
-app = ApplicationBuilder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+bot = Bot(TOKEN)
+print(bot.get_me())
