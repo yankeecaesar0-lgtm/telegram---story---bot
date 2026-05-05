9b81f6a4e40587b55f43088aa912a77f54f1735d
yankeecaesar0-lgtm <yankeecaesar0@gmail.com>
2026-05-05 10:26:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import os
-
+import telegram
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+print("PTB version:", telegram.__version__)
 
-TOKEN = os.getenv("TOKEN")
-
-if TOKEN is None:
-    raise ValueError("TOKEN environment variable not set")
-
-TOKEN = TOKEN.strip()
-
+TOKEN = os.getenv("8594936680:AAEA5qXLCbMRTp8TVUJz-C3Ti4cx_dIzAYQ")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Use /story or /startstory to begin!")
 
+app = ApplicationBuilder().token(TOKEN).build()
 
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
-    app.add_handler(CommandHandler("start", start))
-
-    app.run_polling()
-
-
-if __name__ == "__main__":
-    main()
+app.run_polling()
